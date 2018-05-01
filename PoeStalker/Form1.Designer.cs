8599ff67b6c28b8890994d622d006ca3dc48262a
Piotr Dąbrowski <piotrek816@wp.pl>
2018-05-01 22:52:29 +0200

    Add project files.

---
+++ PoeStalker/Form1.Designer.cs
@@ -0,0 +1,273 @@
+﻿namespace PoeStalker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.buttonPickSet = new System.Windows.Forms.Button();
+            this.buttonRefresh = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxId = new System.Windows.Forms.TextBox();
+            this.comboBoxStashName = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxAcc = new System.Windows.Forms.TextBox();
+            this.dataGridViewStash = new System.Windows.Forms.DataGridView();
+            this.dataGridViewItems = new System.Windows.Forms.DataGridView();
+            this.textBoxStashNo = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewStash)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewItems)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(576, 10);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(65, 59);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "public loop";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Visible = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.textBoxStashNo);
+            this.panel1.Controls.Add(this.checkBox1);
+            this.panel1.Controls.Add(this.buttonPickSet);
+            this.panel1.Controls.Add(this.buttonRefresh);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.textBoxId);
+            this.panel1.Controls.Add(this.comboBoxStashName);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.textBoxAcc);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(650, 81);
+            this.panel1.TabIndex = 2;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(13, 59);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(100, 17);
+            this.checkBox1.TabIndex = 10;
+            this.checkBox1.Text = "Load item icons";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // buttonPickSet
+            // 
+            this.buttonPickSet.Location = new System.Drawing.Point(273, 5);
+            this.buttonPickSet.Margin = new System.Windows.Forms.Padding(2);
+            this.buttonPickSet.Name = "buttonPickSet";
+            this.buttonPickSet.Size = new System.Drawing.Size(65, 59);
+            this.buttonPickSet.TabIndex = 9;
+            this.buttonPickSet.Text = "Pick a set";
+            this.buttonPickSet.UseVisualStyleBackColor = true;
+            this.buttonPickSet.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // buttonRefresh
+            // 
+            this.buttonRefresh.Location = new System.Drawing.Point(196, 5);
+            this.buttonRefresh.Margin = new System.Windows.Forms.Padding(2);
+            this.buttonRefresh.Name = "buttonRefresh";
+            this.buttonRefresh.Size = new System.Drawing.Size(65, 59);
+            this.buttonRefresh.TabIndex = 8;
+            this.buttonRefresh.Text = "refresh stash";
+            this.buttonRefresh.UseVisualStyleBackColor = true;
+            this.buttonRefresh.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(409, 9);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "start id";
+            this.label3.Visible = false;
+            // 
+            // textBoxId
+            // 
+            this.textBoxId.Location = new System.Drawing.Point(452, 7);
+            this.textBoxId.Margin = new System.Windows.Forms.Padding(2);
+            this.textBoxId.Name = "textBoxId";
+            this.textBoxId.Size = new System.Drawing.Size(121, 20);
+            this.textBoxId.TabIndex = 6;
+            this.textBoxId.Text = "180609217-188060193-177101831-203691270-191104426";
+            this.textBoxId.Visible = false;
+            // 
+            // comboBoxStashName
+            // 
+            this.comboBoxStashName.FormattingEnabled = true;
+            this.comboBoxStashName.Location = new System.Drawing.Point(429, 43);
+            this.comboBoxStashName.Margin = new System.Windows.Forms.Padding(2);
+            this.comboBoxStashName.Name = "comboBoxStashName";
+            this.comboBoxStashName.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxStashName.TabIndex = 5;
+            this.comboBoxStashName.Visible = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(367, 46);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Stash:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 10);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(26, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Acc";
+            // 
+            // textBoxAcc
+            // 
+            this.textBoxAcc.Location = new System.Drawing.Point(61, 5);
+            this.textBoxAcc.Margin = new System.Windows.Forms.Padding(2);
+            this.textBoxAcc.Name = "textBoxAcc";
+            this.textBoxAcc.Size = new System.Drawing.Size(99, 20);
+            this.textBoxAcc.TabIndex = 2;
+            this.textBoxAcc.Text = "piotrek816";
+            // 
+            // dataGridViewStash
+            // 
+            this.dataGridViewStash.AllowUserToAddRows = false;
+            this.dataGridViewStash.AllowUserToDeleteRows = false;
+            this.dataGridViewStash.AllowUserToResizeColumns = false;
+            this.dataGridViewStash.AllowUserToResizeRows = false;
+            this.dataGridViewStash.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewStash.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridViewStash.Location = new System.Drawing.Point(0, 81);
+            this.dataGridViewStash.Margin = new System.Windows.Forms.Padding(2);
+            this.dataGridViewStash.Name = "dataGridViewStash";
+            this.dataGridViewStash.RowTemplate.Height = 24;
+            this.dataGridViewStash.Size = new System.Drawing.Size(650, 372);
+            this.dataGridViewStash.TabIndex = 3;
+            this.dataGridViewStash.CellMouseMove += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dataGridView1_CellMouseMove);
+            // 
+            // dataGridViewItems
+            // 
+            this.dataGridViewItems.AllowUserToAddRows = false;
+            this.dataGridViewItems.AllowUserToDeleteRows = false;
+            this.dataGridViewItems.AllowUserToResizeColumns = false;
+            this.dataGridViewItems.AllowUserToResizeRows = false;
+            this.dataGridViewItems.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewItems.Dock = System.Windows.Forms.DockStyle.Right;
+            this.dataGridViewItems.Location = new System.Drawing.Point(412, 81);
+            this.dataGridViewItems.Margin = new System.Windows.Forms.Padding(2);
+            this.dataGridViewItems.Name = "dataGridViewItems";
+            this.dataGridViewItems.RowTemplate.Height = 24;
+            this.dataGridViewItems.Size = new System.Drawing.Size(238, 372);
+            this.dataGridViewItems.TabIndex = 4;
+            // 
+            // textBoxStashNo
+            // 
+            this.textBoxStashNo.Location = new System.Drawing.Point(61, 30);
+            this.textBoxStashNo.Margin = new System.Windows.Forms.Padding(2);
+            this.textBoxStashNo.Name = "textBoxStashNo";
+            this.textBoxStashNo.Size = new System.Drawing.Size(99, 20);
+            this.textBoxStashNo.TabIndex = 11;
+            this.textBoxStashNo.Text = "7";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(9, 33);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(48, 13);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "StashNo";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(650, 453);
+            this.Controls.Add(this.dataGridViewItems);
+            this.Controls.Add(this.dataGridViewStash);
+            this.Controls.Add(this.panel1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewStash)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewItems)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxId;
+        private System.Windows.Forms.ComboBox comboBoxStashName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxAcc;
+        private System.Windows.Forms.Button buttonRefresh;
+        private System.Windows.Forms.DataGridView dataGridViewStash;
+        private System.Windows.Forms.Button buttonPickSet;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.DataGridView dataGridViewItems;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxStashNo;
+    }
+}
+
---
+++ PoeStalker/Form1.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+using System.Threading;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Reflection;
+using WindowsInput.Native;
+using WindowsInput;
+
+namespace PoeStalker
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string charName = "";
+        public List<RootObject> rootObjects = new List<RootObject>();
+        private Stash charStash = new Stash();
+        Dictionary<string, List<StashItemsFiltered>> itemsPerType = new Dictionary<string, List<StashItemsFiltered>>();
+        Dictionary<string, string> itemData = new Dictionary<string, string>();
+        Mouse mouse = new Mouse();
+        Keyboard kb = new Keyboard();
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            itemData = ItemData.FillOutItemData();
+            RefreshStash();
+        }
+
+        private void RefreshStash()
+        {
+            //try
+            {
+                itemsPerType.Clear();
+                itemsPerType = DataConversion.SortItemsToCategories(itemData, WebTools.GetStashItemsFromWeb(textBoxAcc.Text,textBoxStashNo.Text));
+                GridFormating.SetGridRowsColumns(dataGridViewStash, true);
+                GridFormating.SetGridColorsPerItem(itemsPerType, dataGridViewStash, checkBox1.Checked);
+                GridFormating.MakeItemsSummary(itemsPerType, dataGridViewItems);
+            }
+            //catch (Exception ex) { MessageBox.Show("Something went wrong :("+Environment.NewLine+ex.Message+Environment.NewLine+ex.StackTrace); }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            RefreshStash();
+           
+        }
+
+        private StashItemsFiltered copyStashItem(StashItemsFiltered inputItem)
+        {
+            StashItemsFiltered result = new StashItemsFiltered();
+
+                result.category = inputItem.category;
+                result.subCategory = inputItem.subCategory;
+                result.icon = inputItem.icon;
+                result.id = inputItem.id;
+                result.itlvl = inputItem.itlvl;
+                result.typeName = inputItem.typeName;
+                result.x = inputItem.x;
+                result.y = inputItem.y;
+                result.w = inputItem.w;
+                result.h = inputItem.h;
+                result.quadLayout = inputItem.quadLayout;
+                result.picked = inputItem.picked;
+
+            return result;
+        }
+        public class StashItemsFiltered
+        {
+            public string category { get; set; }
+            public string subCategory { get; set; }
+            public string icon { get; set; }
+            public string id { get; set; }
+            public Int32 itlvl { get; set; }
+            public string typeName { get; set; }
+            public Int32 x { get; set; }
+            public Int32 y { get; set; }
+            public Int32 w { get; set; }
+            public Int32 h { get; set; }
+            public bool quadLayout { get; set; }
+            public bool picked { get; set; }
+        }
+
+        public class PrivateStash
+        {   
+            public bool quadLayout { get; set; }
+            public List<object> items { get; set; }
+        }
+
+        public class Stash
+        {
+            public string accountName { get; set; }
+            public string lastCharacterName { get; set; }
+            public string id { get; set; }
+            public string stash { get; set; }
+            public string stashType { get; set; }
+            public List<object> items { get; set; }
+            public bool @public { get; set; }
+        }
+        
+        public class RootObject
+        {
+            public string next_change_id { get; set; }
+            public List<Stash> stashes { get; set; }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            charName = textBoxAcc.Text;
+
+            new Thread(() =>
+            {
+                Thread.CurrentThread.IsBackground = true;
+                PublicStash.fillRootObjects(charStash, textBoxAcc.Text, PublicStash.GetLatestId());
+            }).Start();
+
+            
+        }
+        
+        private void button3_Click(object sender, EventArgs e)
+        {
+            List<StashItemsFiltered> set = null; 
+            int setsNo = SetCalculation.HowManySets(itemsPerType);
+            if (setsNo > 0)
+            {
+                Dictionary<string, List<StashItemsFiltered>> itemsPerTypeCopy = new Dictionary<string, List<StashItemsFiltered>>();
+                foreach (var typeEntry in itemsPerType)
+                {
+                    List<StashItemsFiltered> newList = new List<StashItemsFiltered>();
+                    foreach (StashItemsFiltered item in typeEntry.Value)
+                    {
+                        StashItemsFiltered newItem = copyStashItem(item);
+                        newList.Add(newItem);
+                    }
+                    itemsPerTypeCopy.Add(typeEntry.Key, newList);
+                }
+
+                set = SetCalculation.MakeSets(itemsPerTypeCopy);
+
+                kb.SendDown(Keys.ControlKey);
+                {
+                    foreach (StashItemsFiltered item in set)
+                    {
+                        Tuple<int, int> click = Coordinates.GetClickCoordinates(item.x, item.y, item.quadLayout);
+                        Thread.Sleep(50);
+                        mouse.MouseLeftClick(click.Item1, click.Item2);
+                    }
+                }
+                kb.SendUp(Keys.ControlKey);
+
+                Thread.Sleep(2000);
+                RefreshStash();
+            }
+        }
+
+        private void dataGridView1_CellMouseMove(object sender, DataGridViewCellMouseEventArgs e)
+        {
+
+        }
+    }
+}
+
